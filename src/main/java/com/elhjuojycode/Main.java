0fb1828bc6004264430264a3d142afcad6c8eeef
elhajuojy <souirimehdi311@gmail.com>
2023-04-29 11:52:18 +0100

    clean up the project and start from ground up , by adding basic spring boot elements

--- src/main/java/com/elhjuojycode/Main.java
+++ src/main/java/com/elhjuojycode/Main.java
@@ -1,13 +1,13 @@
 package com.elhjuojycode;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootExampleApplication {
+public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
 }
