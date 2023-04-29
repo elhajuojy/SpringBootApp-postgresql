c140e20454b56c84bae8c0a102f62a57c663ab9b
elhajuojy <souirimehdi311@gmail.com>
2023-04-29 11:41:47 +0100

    Initial commit

---
+++ src/main/java/com/elhjuojycode/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.elhjuojycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
